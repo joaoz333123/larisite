ad277385392a878a5c09a6047da09ddb203f2be6
joaoz333123 <182572330+joaoz333123@users.noreply.github.com>
2026-08-03 23:06:37 -0300

    Atualização de produtos via Painel Admin

--- js/products.js
+++ js/products.js
@@ -69,11 +69,11 @@ const products = [
         "subtitle": "Pink Gold EDP 100ml",
         "category": "perfumes",
         "badge": "Importado Orig.",
-        "price": 149.9,
+        "price": 119.9,
         "image": "https://parfumneked.hu/parfumneked/wp-content/uploads/2019/12/MASTER-pink-gold.jpg",
         "status": "available",
         "isOnSale": true,
-        "originalPrice": 119.9
+        "originalPrice": 149.9
     },
     {
         "id": 7,
